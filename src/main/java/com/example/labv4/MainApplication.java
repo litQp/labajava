643c38d8ed9d77eca17d2ef22d79e499a99409a2
litovsky <40055444+litQp@users.noreply.github.com>
2023-11-03 09:27:28 +0300

    laba 6

--- src/main/java/com/example/labv4/MainApplication.java
+++ src/main/java/com/example/labv4/MainApplication.java
@@ -2,14 +2,18 @@ package com.example.labv4;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.util.Objects;
+
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 700);
-        stage.setTitle("Лаб 5");
+        stage.setTitle("Лаб 6");
+        stage.getIcons().add(new Image(Objects.requireNonNull(MainApplication.class.getResourceAsStream("/com/example/labv4/images/logo.png"))));
         stage.setScene(scene);
         stage.show();
     }
