686e8abdf7e36a417d1fa7b6c9fd4b059b26e61f
litovsky <40055444+litQp@users.noreply.github.com>
2023-11-03 09:22:24 +0300

    laba 3

---
+++ src/main/java/com/example/labs3/MainApplication.java
@@ -0,0 +1,29 @@
+package com.example.labs3;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
+        stage.setTitle("Лаб 3");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
+
+
+
+
+
+
+
+
+
